02c82d43da96a21607f08decb2f030ca4f7d9031
DrM4D <weonic@yandex.ru>
2017-10-21 22:19:46 +0300

    Version 1.3a
    215 lines were formatted

--- src/main/java/com/drm4d/investment/Main.java
+++ src/main/java/com/drm4d/investment/Main.java
@@ -3,30 +3,27 @@ package com.drm4d.investment;
 import java.util.*;
 import java.math.*;
 
-public class Main
-{
-  public static void main(String[] args)
-  {
-    String version = "1.3";
-    String systemVersion = "J1AA4F21OC17";
-    SystemC s = new SystemC();
-    s.intro();
-    Main.start();
-  }
-  
-  public static void start()
-  {
-    SystemC s = new SystemC();
-    
-    s.inputStSm();
-    s.inputPercent();
-    s.getAct();
-    
-    if (s.toDo == 1) s.inputFnSm();
-    else s.inputTime();
-    
-    s.calculate();
-    s.askRestart();
+public class Main {
+    public static void main(String[] args) {
+        String version = "1.3a";
+        String systemVersion = "J1AA5F21OC17";
+        SystemC s = new SystemC();
+        s.intro();
+        Main.start();
+    }
 
-  }
+    public static void start() {
+        SystemC s = new SystemC();
+
+        s.inputStSm();
+        s.inputPercent();
+        s.getAct();
+
+        if (s.toDo == 1) s.inputFnSm();
+        else s.inputTime();
+
+        s.calculate();
+        s.askRestart();
+
+    }
 }
